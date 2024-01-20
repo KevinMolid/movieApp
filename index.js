5a5502ffdd93712d84442300cc073790221985ff
Kevin Molid <kevinmolid@gmail.com>
2024-01-20 19:16:02 +0100

    Styled app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Imports */
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js"
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js'
 
 /* Firebase setup */
 const firebaseConfig = {
@@ -13,8 +13,78 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
-console.log(app)
+console.log(auth)
 
 /* UI Elements */
 const viewLoggedOut = document.getElementById("logged-out-view")
 const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+/* === Main Code === */
+
+showLoggedOutView()
+
+/* Functions */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+    console.log("Sign in with Google")
+}
+
+function authSignInWithEmail() {
+    console.log("Sign in with email and password")
+}
+
+function authCreateAccountWithEmail() {
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            showLoggedInView() 
+        })
+        .catch((error) => {
+            console.error(error.message)
+        })
+    /*  Challenge:
+       
+        If the creation of user is successful then you should show the logged in view using showLoggedInView()
+        If something went wrong, then you should log the error message using console.error.
+    */
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+    hideElement(viewLoggedIn)
+    showElement(viewLoggedOut)
+}
+
+function showLoggedInView() {
+    hideElement(viewLoggedOut)
+    showElement(viewLoggedIn)
+}
+
+function showElement(element) {
+    element.style.display = "flex"
+}
+
+function hideElement(element) {
+    element.style.display = "none"
+}
